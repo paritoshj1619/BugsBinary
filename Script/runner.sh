aa59987300d9873f493474b8a2ce5f406277de2f
paritoshj1619 <83442697+paritoshj1619@users.noreply.github.com>
2022-03-31 12:12:39 +0530

    Add files via upload

--- Script/runner.sh
+++ Script/runner.sh
@@ -24,11 +24,10 @@ if [[ $1 == "Darwin" ]]; then
     $BugsScrcpy_Bin/app/BugsScrcpy -s $2 --tunnel-host=$4 --tunnel-port=27185
 fi
 if [[ $1 == "MSYS" ]]; then
-    set BugsAdb_Bin=$currPath/bin/BugsAdb
-    set BugsScrcpy_Bin=$currPath/bin/BugsScrcpy
-    set PATH="$PATH:$BugsAdb_Bin:$BugsScrcpy_Bin"
-    set token=$3
-    set ADB_SERVER_SOCKET=tcp:$4:5038
-    echo PATH
+    export BugsAdb_Bin=$currPath/bin/BugsAdb
+    export BugsScrcpy_Bin=$currPath/bin/BugsScrcpy
+    export PATH="$PATH:$BugsAdb_Bin:$BugsScrcpy_Bin"
+    export token=$3
+    export ADB_SERVER_SOCKET=tcp:$4:5038
     $BugsScrcpy_Bin/BugsScrcpy.exe -s $2 --tunnel-host=$4 --tunnel-port=27185
 fi
