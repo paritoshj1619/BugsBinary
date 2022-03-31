2e5e637173346f316bf066cbce9f403e74793ae6
paritoshj1619 <83442697+paritoshj1619@users.noreply.github.com>
2022-03-31 12:43:34 +0530

    Update installation.sh

--- Script/installation.sh
+++ Script/installation.sh
@@ -3,13 +3,13 @@
 DIR="$(pwd)/bin"
 if [[ $1 == "Linux" ]]; then
     if [ ! -d "$DIR" ]; then
-        wget "https://github.com/paritoshj1619/BugsBinary/raw/master/Linux/bin.zip"
+        curl -OL "https://github.com/paritoshj1619/BugsBinary/raw/master/Linux/bin.zip"
         unzip bin.zip
         rm -r bin.zip
     fi
 elif [[ $1 == "MSYS" ]]; then
     if [ ! -d "$DIR" ]; then
-        powershell -command "Invoke-WebRequest -Uri https://github.com/paritoshj1619/BugsBinary/raw/master/Windows/bin.zip -OutFile bin.zip"
+        curl -OL "https://github.com/paritoshj1619/BugsBinary/raw/master/Windows/bin.zip"
         powershell -command "Expand-Archive bin.zip"
         powershell -command "rm -r bin.zip"
     fi
